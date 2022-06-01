1463fba61fca5c9579f339e5c413e3a99f111c6a
gettsige <gtwold46@gmail.com>
2022-06-01 19:52:54 -0400

    for loop commit

--- WedaftPrac2/src/comaftPrac1/LDdemo1.java
+++ WedaftPrac2/src/comaftPrac1/LDdemo1.java
@@ -5,6 +5,14 @@ public class LDdemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		for (int k = 5; k < 10; k++) {
+
+			for (int i = 1; i <= 5; i++) {
+				System.out.print("*" + "  ");
+			}
+
+			System.out.println();
+		}
 	}
 
 }
