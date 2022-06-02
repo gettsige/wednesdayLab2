81a6d95565c0fd7ddc2352018d578db5e5e2d0e1
gettsige <gtwold46@gmail.com>
2022-06-01 20:03:57 -0400

    while commit

--- WedaftPrac1/src/comaftPrac/Ldemo3.java
+++ WedaftPrac1/src/comaftPrac/Ldemo3.java
@@ -5,6 +5,16 @@ public class Ldemo3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		
+		int k =1;
+		while(k<=4) {
 
+		int  i = 1;	
+		while( i<=5) {
+			System.out.print( i + " "); i++;
+		}
+		System.out.println(); k++;	
+		
+		}
+	}
 }
